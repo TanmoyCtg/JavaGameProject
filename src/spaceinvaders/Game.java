f72c93b37ec8d627b038bb823e2296592cac0c57
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-04 17:08:36 +0600

    change the Entity.java add setHorizontalMovement, move function

--- src/spaceinvaders/Game.java
+++ src/spaceinvaders/Game.java
@@ -1,4 +1,122 @@
 package spaceinvaders;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
 
-public class Game {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Game extends Canvas{
+    /** The stragey that allows us to use accelerate page flipping */
+    private BufferStrategy strategy;
+    /** True if the game is currently "running", i.e. the game loop is looping */
+    private boolean gameRunning = true;
+    /** The list of all the entities that exist in our game */
+    private ArrayList entities = new ArrayList();
+    /** The list of entities that need to be removed from the game this loop */
+    private ArrayList removeList = new ArrayList();
+    /** The entity representing the player */
+    private Entity ship;
+    /** The speed at which the player's ship should move (pixels/sec) */
+    private double moveSpeed = 300;
+    /** The time at which last fired a shot */
+    private long lastFire = 0;
+    /** The interval between our players shot (ms) */
+    private long firingInterval = 500;
+    /** The number of aliens left on the screen */
+    private int alienCount;
+
+    /** The message to display which waiting for a key press */
+    private String message = "";
+    /** True if we're holding up game play until a key has been pressed */
+    private boolean waitingForKeyPress = true;
+    /** True if the left cursor key is currently pressed */
+    private boolean leftPressed = false;
+    /** True if the right cursor key is currently pressed */
+    private boolean rightPressed = false;
+    /** True if we are firing */
+    private boolean firePressed = false;
+    /** True if game logic needs to be applied this loop, normally as a result of a game event */
+    private boolean logicRequiredThisLoop = false;
+
+    /**
+     * Construct our game and set it running.
+     */
+    public Game() {
+        // create a frame to contain our game
+        JFrame container = new JFrame("Space Invaders 101");
+
+        // get hold the content of the frame and set up the resolution of the game
+        JPanel panel = (JPanel) container.getContentPane();
+        panel.setPreferredSize(new Dimension(800,600));
+        panel.setLayout(null);
+
+        // setup our canvas size and put it into the content of the frame
+        setBounds(0,0,800,600);
+        panel.add(this);
+
+        // Tell AWT not to bother repainting our canvas since we're
+        // going to do that our self in accelerated mode
+        setIgnoreRepaint(true);
+
+        // finally make the window visible
+        container.pack();
+        container.setResizable(false);
+        container.setVisible(true);
+
+        // add a listener to respond to the user closing the window. If they
+        // do we'd like to exit the game
+        container.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // add a key input system (defined below) to our canvas
+        // so we can respond to key pressed
+        addKeyListener(new KeyInputHandler());
+
+        // request the focus so key events come to us
+        requestFocus();
+
+        // create the buffering strategy which will allow AWT
+        // to manage our accelerated graphics
+        createBufferStrategy(2);
+        strategy = getBufferStrategy();
+
+        // initialise the entities in our game so there's something
+        // to see at startup
+        initEntities();
+    }
+
+    /**
+     * Start a fresh game, this should clear out any old data and
+     * create a new set.
+     */
+    private void startGame() {
+        // clear out any existing entities and intialise a new set
+        entities.clear();
+        initEntities();
+
+        // blank out any keyboard settings we might currently have
+        leftPressed = false;
+        rightPressed = false;
+        firePressed = false;
+    }
+
+    public static void main(String argv[]) {
+        Game g =new Game();
+
+        // Start the main game loop, note: this method will not
+        // return until the game has finished running. Hence we are
+        // using the actual main thread to run the game.
+        //g.gameLoop();
+    }
 }
+
