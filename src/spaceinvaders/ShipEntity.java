6a1d0f801c940029224f0404090b202c6875dec9
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2017-12-09 23:59:46 +0600

    finish ShipEntity.java (#9)

--- src/spaceinvaders/ShipEntity.java
+++ src/spaceinvaders/ShipEntity.java
@@ -1,5 +1,25 @@
 package spaceinvaders;
 
-
-public class ShipEntity {
+// the entity represents the players ship
+public class ShipEntity extends Entity {
+    // the game in which the ship exists
+    private Game game;
+    public ShipEntity(Game game,String ref,int x, int y){
+        super (ref,x,y);
+        this.game = game;
+    }
+    public void move(long delta){
+        if ((dx < 0) && (x < 10)){
+            return;
+        }
+        if ((dx > 0) && (x > 755)){
+            return;
+        }
+        super.move(delta);
+    }
+    public void collidedWith(Entity other){
+        if (other instanceof  AlienEntity){
+            game.notifyDeath();
+        }
+    }
 }
